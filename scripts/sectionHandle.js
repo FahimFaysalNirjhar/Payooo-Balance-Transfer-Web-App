785086a649220c363281333515c566ad6481a1f8
Fahim Faysal Nirjhar <fahimfaysal1995@gmail.com>
2025-12-07 22:47:41 +0600

    add money display worked properly

---
+++ scripts/sectionHandle.js
@@ -0,0 +1,7 @@
+// add money
+let addMoneySection = document.getElementById("addMoneySection");
+addMoneySection.style.display = "none";
+let addMoney = document.getElementById("addMoney");
+addMoney.addEventListener("click", () => {
+  addMoneySection.style.display = "block";
+});
