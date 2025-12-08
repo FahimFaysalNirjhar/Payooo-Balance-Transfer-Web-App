dec51484a1dfcfe116d3562d38c080e3f2480247
Fahim Faysal Nirjhar <fahimfaysal1995@gmail.com>
2025-12-08 16:01:44 +0600

    cashout toggle complete

--- scripts/toggle.js
+++ scripts/toggle.js
@@ -1,18 +1,34 @@
 // add money
 let addMoneySection = document.getElementById("addMoneySection");
 addMoneySection.style.display = "none";
+
 // transaction
 let transactionSection = document.getElementById("transaction-Section");
 transactionSection.style.display = "none";
+
+// cash out
+let cashOutSection = document.getElementById("cashout-section");
+cashOutSection.style.display = "none";
+
 //add money
 let addMoney = document.getElementById("addMoney");
 addMoney.addEventListener("click", () => {
   addMoneySection.style.display = "block";
   transactionSection.style.display = "none";
 });
+
 //transaction
 let transaction = document.getElementById("transaction");
 transaction.addEventListener("click", () => {
   transactionSection.style.display = "block";
   addMoneySection.style.display = "none";
+  cashOutSection.style.display = "none";
+});
+
+//chas out
+let cashOut = document.getElementById("CashOut");
+cashOut.addEventListener("click", () => {
+  addMoneySection.style.display = "none";
+  transactionSection.style.display = "none";
+  cashOutSection.style.display = "block";
 });
